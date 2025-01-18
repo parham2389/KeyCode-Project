1b5a52816d3927c36f6e60c017c9dd7f6ac8ed3c
Parham <155875539+parham2389@users.noreply.github.com>
2025-01-17 21:14:30 -1200

    Commit-1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+﻿let $ = document
+let asciiCode = $.querySelector('#keyCode')
+let key = $.querySelector('#key')
+let locationKey = $.querySelector('#location')
+let which = $.querySelector('#which')
+let code = $.querySelector('#code')
+let title = $.querySelector('title')
+
+document.body.addEventListener('keydown', function (event) {
+	asciiCode.innerHTML = event.keyCode
+	key.innerHTML = event.key
+	locationKey.innerHTML = event.location
+	which.innerHTML = event.which
+	code.innerHTML = event.code
+	// console.log(event);
+	event.preventDefault()
+	starter.style.display = 'none'
+	heading.style.display = 'flex'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+})
